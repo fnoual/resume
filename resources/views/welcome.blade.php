19208310471e1938cec8a8fc51f526115295ac7f
Fawzi Noual <fouzi.noual@gmail.com>
2023-05-17 07:16:40 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/css/all.min.css','resources/js/app.js'])
+    <title>Fawzi NOUAL - Web developement</title>
+</head>
+<body class="h-full bg-gradient-to-r from-rose-100 to-teal-100 background-animate flex justify-center items-center">
+    <div class="container bg-white rounded shadow-sm p-10">
+        <h3 class="text-5xl uppercase font-bold">
+            Fawzi NOUAL
+        </h3>
+        <h3 class="text-2xl  font-semibold text-slate-500">
+            Développeur web full stack
+        </h3>
+
+        <p class="my-4">
+           Passioné de tech depuis toujours, j'ai vite compris que la plus grande invention de notre histoire était bien internet ,
+           naturellement tourné vers le développement web, j'ai commencé par faire des modules pour PhpBB et vBulletin et d'année
+           en année je me suis spécialisé dans le développement de manière plus académique et complexe jusqu'a travailler avec les
+           acteurs les plus pertinents de l'industrie pharmaceutique , financière et sanitaire en délivrant des produits et services
+           respectant leurs standards  exigeants en matière de qualité et de sécurité .
+        </p>
+
+
+        <h3 class="text-2xl  font-semibold">
+            Expérience
+        </h3>
+        <div class="flex justify-evenly mb-10">
+            <div class="div w-full">
+                <h3 class="text-l  font-light text-slate-500 mt-4">
+                    Déc. 2022 - Auj.
+                </h3>
+                <h3 class="text-l  font-semibold text-slate-500">
+                    Développeur web Sénior - Beyn
+                </h3>
+
+            </div>
+            <div class="div w-full">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fa-brands fa-laravel"></i></span>PHP 8 - Laravel 9</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-database"></i></span>PostgreSQL</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-vuejs"></i></span>VueJS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-css3"></i></span>TailwindCSS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-jira"></i></span>Jira</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-docker"></i></span>Docker </li>
+                    <li><span class="fa-li"><i class="fa-brands fa-gitlab"></i></span>Gitlab (vcs)</li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex justify-evenly mb-10">
+            <div class="div w-full">
+                <h3 class="text-l  font-light text-slate-500 mt-4">
+                    Aoû. 2022 - Déc. 2022
+                </h3>
+                <h3 class="text-l  font-semibold text-slate-500">
+                    Développeur web full stack - Instant Systems ( Mobdev )
+                </h3>
+            </div>
+            <div class="div w-full">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fa-brands fa-symfony"></i></span>PHP 7 - Symfony 5</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-mysql"></i></span>MySQL - MariaDB</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-bootstrap"></i></span>Bootstrap</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-jenkins"></i></span>Jenkins</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-jira"></i></span>Jira</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-bitbucket"></i></span>Bitbucket (vcs)</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-confluence"></i></span>Confluence</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-docker"></i></span>Docker </li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex justify-evenly mb-10">
+            <div class="div w-full">
+                <h3 class="text-l  font-light text-slate-500 mt-4">
+                    Oct. 2017 - Aoû. 2022
+                </h3>
+                <h3 class="text-l  font-semibold text-slate-500">
+                    Chef de projet web - Lotus Médias
+                </h3>
+
+            </div>
+            <div class="div w-full">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fa-brands fa-laravel"></i></span>PHP 7/8 - Laravel 9</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-vuejs"></i></span>VueJS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-react"></i></span>React JS / RN</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-css3"></i></span>TailwindCSS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-bootstrap"></i></span>Bootstrap</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-gitlab"></i></span>Gitlab (vcs)</li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex justify-evenly mb-10">
+            <div class="div w-full">
+                <h3 class="text-l  font-light text-slate-500 mt-4">
+                    Nov. 2014 - Oct. 2017
+                </h3>
+                <h3 class="text-l  font-semibold text-slate-500">
+                    Développeur web , Ingénieur SI - SPA Groupe ETRHB Haddad
+                </h3>
+
+            </div>
+            <div class="div w-full">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fa-brands fa-php"></i></span>PHP 5/7</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-ubuntu"></i></span>Ubuntu</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-js-square"></i></span>Vanilla JS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-odoo"></i></span>TailwindCSS</li>
+                    <li><span class="fa-li"><i class="fa-brands fa-gitlab"></i></span>Gitlab (vcs)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
